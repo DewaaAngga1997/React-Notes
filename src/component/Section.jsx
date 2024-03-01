a68a087ff63f7f39ed6f766e95984a5e4562d62d
Dewa Angga PC <devaangga53@gmail.com>
2024-03-02 07:48:09 +0800

    add core_concept.map

--- src/component/Section.jsx
+++ src/component/Section.jsx
@@ -16,29 +16,9 @@ function Section() {
     <section id="core-concepts">
       <h2>Core Concepts</h2>
       <ul>
-        <CoreConcept
-          image={CORE_CONCEPTS[0].image}
-          title={CORE_CONCEPTS[0].title}
-          description={CORE_CONCEPTS[0].description}
-        />
-
-        <CoreConcept
-          image={CORE_CONCEPTS[1].image}
-          title={CORE_CONCEPTS[1].title}
-          description={CORE_CONCEPTS[1].description}
-        />
-
-        <CoreConcept
-          image={CORE_CONCEPTS[2].image}
-          title={CORE_CONCEPTS[2].title}
-          description={CORE_CONCEPTS[2].description}
-        />
-
-        <CoreConcept
-          image={CORE_CONCEPTS[3].image}
-          title={CORE_CONCEPTS[3].title}
-          description={CORE_CONCEPTS[3].description}
-        />
+        {CORE_CONCEPTS.map((item) => (
+          <CoreConcept key={item.title} {...item} />
+        ))}
       </ul>
     </section>
   );
